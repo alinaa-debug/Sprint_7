7ef35f52c08bf0b5241ef809828dfad41b1ee978
Alina Rakhimiyanova <maybee.alina1998@gmail.com>
2026-04-14 19:26:37 +0300

    My 7th ptoject

---
+++ data.py
@@ -0,0 +1,17 @@
+
+import random
+import string
+
+class Url:
+    Base_url = 'https://qa-scooter.praktikum-services.ru'
+    Create_url = '/api/v1/courier'
+    Login_url = '/api/v1/courier/login'
+    Create_order = '/api/v1/orders'
+    
+
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))  
+
+    
---
+++ tests/test_create_order.py
@@ -0,0 +1,26 @@
+import pytest 
+import requests
+from data import Url
+
+class CreateOrder():
+    @pytest.mark.parametrize("colors", [ 
+    ["BLACK"],
+    ["GRAY"],
+    ["BLACK","GRAY"],
+    [] ])
+    def test_create_order(self,colors):
+        data = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": colors
+}
+    
+        response = requests.post(f'{Url.Base_url}{Url.Create_order}',json=data )
+        assert response.status_code == 200
+        assert 'track' in response.json()
